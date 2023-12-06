d3b393edc0506ec137c0a9e7cd3182d04406fb06
NikVio <131026216+NikVio@users.noreply.github.com>
2023-12-06 02:55:17 +0200

    reload

--- src/components/Pages/MovieDetails.js
+++ src/components/Pages/MovieDetails.js
@@ -1,25 +1,25 @@
-import { fetchDetailsMovies } from 'components/MoviesService';
-import { useEffect, useState } from 'react';
+// import { fetchDetailsMovies } from 'components/MoviesService';
+// import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 export default function MovieDetails() {
   const params = useParams();
   console.log(params);
 
-  const [detailsMovie, setDetailsMovie] = useState(null);
+  //const [detailsMovie, setDetailsMovie] = useState(null);
 
-  useEffect(() => {
-    async function getDetails() {
-      try {
-        const fetchDetails = await fetchDetailsMovies(params.movieId);
-        setDetailsMovie(fetchDetails);
-      } catch (error) {
-        console.log('error');
-      }
-    }
-    getDetails();
-  }, [params.movieId]);
-  return <div>Movies</div>;
+  // useEffect(() => {
+  //   async function getDetails() {
+  //     try {
+  //       const fetchDetails = await fetchDetailsMovies(params.movieId);
+  //       setDetailsMovie(fetchDetails);
+  //     } catch (error) {
+  //       console.log('error');
+  //     }
+  //   }
+  //   getDetails();
+  // }, [params.movieId]);
+  // return <div>Movies</div>;
   // <ul>
   //   {detailsMovie.map(data => {
   //     const {
