6ce3f06a3f3404f8d6bd88973149215e94b85e99
NikVio <131026216+NikVio@users.noreply.github.com>
2023-12-12 22:59:03 +0000

    Deploying to gh-pages from @ NikVio/goit-react-hw-05-movies@50ed1f482ea8986cd64bd3a7cf3f0453c4634d4c 🚀

---
+++ static/js/982.0be1fcab.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[982],{6982:function(e,r,t){t.r(r),t.d(r,{default:function(){return u}});var n=t(1087),o=t(184);function u(){return(0,o.jsx)("div",{children:(0,o.jsxs)("p",{children:["Sorry! Page not found. You can return to our ",(0,o.jsx)(n.rU,{to:"/",children:"Home"})]})})}}}]);
+//# sourceMappingURL=982.0be1fcab.chunk.js.map
--- static/js/982.9be21214.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[982],{982:function(e,r,t){t.r(r),t.d(r,{default:function(){return u}});var n=t(87),o=t(184);function u(){return(0,o.jsx)("div",{children:(0,o.jsxs)("p",{children:["Sorry! Page not found. You can return to our ",(0,o.jsx)(n.rU,{to:"/",children:"Home"})]})})}}}]);
-//# sourceMappingURL=982.9be21214.chunk.js.map
